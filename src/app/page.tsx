b84a67cfd1cfed3c08894e0c7bf756bf394f7d81
Felix-Silva <felixsilva316@outlook.com>
2026-07-29 21:29:31 -0500

    change page render type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import { supabase } from "@/lib/supabase";
 import { storeColorBackground, type Store } from "@/lib/stores";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const { data: stores, error } = await supabase
     .from("stores")
